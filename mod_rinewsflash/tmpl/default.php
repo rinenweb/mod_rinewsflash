f0b861187495ed999ca42a8710fd18c0e4a42b97
Ioannis Fytros <rinenweb@gmail.com>
2024-02-12 20:39:05 +0200

    Update default.php

--- mod_rinewsflash/tmpl/default.php
+++ mod_rinewsflash/tmpl/default.php
@@ -5,28 +5,36 @@
  * @license    GNU General Public License v2
  * @link       https://www.rinenweb.eu
  */
-
 // No direct access to this file
 defined('_JEXEC') or die; ?>
 <?php
-// Import the Route class
 use Joomla\CMS\Router\Route;
 
-// Build the URL to the article
 $articleUrl = Route::_('index.php?option=com_content&view=article&id=' . $articleId);
 ?>
-<div class="latest-additions">
-    <?php foreach ($list as $item): ?>
-        <div><?php echo $item; ?></div>
-        <?php if (next($list)): // Check if there is another item in the array ?>
-            <hr />
+<table class="latest-additions">
+    <?php foreach ($list as $index => $item): ?>
+        <tr>
+            <td>
+                <?php echo $item; ?>
+            </td>
+        </tr>
+        <?php if ($index < count($list) - 1): // Check if there is another item in the array ?>
+            <tr>
+                <td>
+                    <hr />
+                </td>
+            </tr>
         <?php endif; ?>
     <?php endforeach; ?>
 
     <?php if ($showLink == 1): ?>
-        <p style="text-align: center;"><a href="<?php echo $articleUrl; ?>" class="<?php echo htmlspecialchars($linkClass, ENT_QUOTES, 'UTF-8'); ?>">
-            <?php echo htmlspecialchars($linkText, ENT_QUOTES, 'UTF-8'); ?>
-        </a></p>
+        <tr>
+            <td style="text-align: center;">
+                <a href="<?php echo $articleUrl; ?>" class="<?php echo htmlspecialchars($linkClass, ENT_QUOTES, 'UTF-8'); ?>">
+                    <?php echo htmlspecialchars($linkText, ENT_QUOTES, 'UTF-8'); ?>
+                </a>
+            </td>
+        </tr>
     <?php endif; ?>
-</div>
-
+</table>
